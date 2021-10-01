7c671429df8c6b33b49a3adf688eed5150c3eade
Vision-Tey <nimusiimavision@gmail.com>
2021-10-01 09:52:17 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Home from './Page/Home'
+import Nav from './Components/Nav'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+  <Nav/>    
+  <Home/>
+  <Footer/>
+    </div>
+  );
+}
+
+export default App;
